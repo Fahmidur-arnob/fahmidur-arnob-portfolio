5c046cd6daa8cb53e2dfe087e409a0d6cd47483a
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-18 11:23:41 +0600

    Changed some directories

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 export default [
     {
         title: 'Book Depo',
-        imgUrl: '../../public/assets/bookdepo.png',
+        imgUrl: '../assets/bookdepo.png',
         stack: ['HTML5', 'TailwindCSS(DaisyUI)', 'ReactJS','React-Router-Dom', 'NodeJS', 'ExpressJS', 'MongoDB', 'Firebase'],
         client_side_code: 'https://github.com/Fahmidur-arnob/Book_Depo',
         server_side_code: 'https://github.com/Fahmidur-arnob/Book_Depo_Server',
@@ -9,7 +9,7 @@ export default [
     },
     {
         title: 'The Picture and The Poet',
-        imgUrl: '../../public/assets/poet.png',
+        imgUrl: '../assets/poet.png',
         stack: ['HTML5', 'TailwindCSS(DaisyUI)', 'ReactJS','React-Router-Dom', 'NodeJS', 'ExpressJS', 'MongoDB', 'Firebase'],
         client_side_code:'https://github.com/Fahmidur-arnob/The_Picture_And_The_Poet',
         server_side_code: 'https://github.com/Fahmidur-arnob/The_Picture_And_The_Poet_Server',
@@ -17,14 +17,14 @@ export default [
     },
     {
         title: 'Meal Finder',
-        imgUrl: '../../public/assets/mealfinder.png',
+        imgUrl: '../assets/mealfinder.png',
         stack: ['HTML5', 'CSS3', 'Javascript'],
         client_side_code: 'https://github.com/Fahmidur-arnob/Meal-Finder',
         link: 'https://serene-capybara-73633d.netlify.app/',
     },
     {
         title: 'Online Shopping App',
-        imgUrl: '../../public/assets/shopping.png',
+        imgUrl: '../assets/shopping.png',
         stack: ['HTML5', 'TailwindCSS', 'ReactJs',],
         client_side_code: 'https://github.com/Fahmidur-arnob/Shopping_App',
         link: 'https://nimble-queijadas-039f8b.netlify.app/',
