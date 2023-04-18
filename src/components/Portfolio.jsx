71b9e3d792bbe34b698bb54d130ba3f7ec082114
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-18 12:07:04 +0600

    Done

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,7 +8,7 @@ function Portfolio() {
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {portfolio.map(project => (
                <PortfolioCardItem 
-                  imgUrl={project.img_url}
+                  img_url={project.img_url}
                   title={project.title}
                   stack={project.stack}
                   link={project.link}
