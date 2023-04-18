55adc1c1d035537ce8ef34dde09fb1764307eae2
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-18 12:24:37 +0600

    Added LinkedIN

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
                 <div>
                     <div className="flex flex-wrap justify-center gap-2">
                         <a
-                            href="www.linkedin.com/in/fahmidur-arnob"
+                            href="https://www.linkedin.com/in/fahmidur-arnob/"
                             className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
                             <svg
                                 className="w-5 h-5 fill-current"
