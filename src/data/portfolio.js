2008e5bbc5836c0f0cffdfc4fe369e55d1cdcb3f
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-18 12:01:14 +0600

    Uploaded images on imgbb

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 export default [
     {
         title: 'Book Depo',
-        img_url: "../../public/assets/bookdepo.png",
+        img_url: "https://i.ibb.co/Y80Zgp9/bookdepo.png",
         stack: ['HTML5', 'TailwindCSS(DaisyUI)', 'ReactJS','React-Router-Dom', 'NodeJS', 'ExpressJS', 'MongoDB', 'Firebase'],
         client_side_code: 'https://github.com/Fahmidur-arnob/Book_Depo',
         server_side_code: 'https://github.com/Fahmidur-arnob/Book_Depo_Server',
@@ -9,7 +9,7 @@ export default [
     },
     {
         title: 'The Picture and The Poet',
-        img_url: "../../public/assets/poet.png",
+        img_url: "https://i.ibb.co/2PTfGh2/poet.png",
         stack: ['HTML5', 'TailwindCSS(DaisyUI)', 'ReactJS','React-Router-Dom', 'NodeJS', 'ExpressJS', 'MongoDB', 'Firebase'],
         client_side_code:'https://github.com/Fahmidur-arnob/The_Picture_And_The_Poet',
         server_side_code: 'https://github.com/Fahmidur-arnob/The_Picture_And_The_Poet_Server',
@@ -17,14 +17,14 @@ export default [
     },
     {
         title: 'Meal Finder',
-        img_url: "../../public/assets/mealfinder.png",
+        img_url: "https://i.ibb.co/H73bGXZ/mealfinder.png",
         stack: ['HTML5', 'CSS3', 'Javascript'],
         client_side_code: 'https://github.com/Fahmidur-arnob/Meal-Finder',
         link: 'https://serene-capybara-73633d.netlify.app/',
     },
     {
         title: 'Online Shopping App',
-        img_url: "../../public/assets/shopping.png",
+        img_url: "https://i.ibb.co/ThKcwp2/shopping.png",
         stack: ['HTML5', 'TailwindCSS', 'ReactJs',],
         client_side_code: 'https://github.com/Fahmidur-arnob/Shopping_App',
         link: 'https://nimble-queijadas-039f8b.netlify.app/',
